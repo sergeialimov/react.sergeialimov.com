9fe1c80d7953ba532717f1bf1101e43526666dc8
Sergei Alimov <8912652@gmail.com>
2018-12-23 21:01:30 +0300

    add styles to body

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import Header from './header.jsx';
-// import './../styles/app.css';
 
 class App extends Component {
+  componentDidMount() {
+    document.body.style.margin = '0';
+  }
   render() {
     return (
       <div className="app">
