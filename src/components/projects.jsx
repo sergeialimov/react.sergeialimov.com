1c0c82391a0056677cd50c4913c60b99f6c04f8f
Sergei Alimov <8912652@gmail.com>
2018-12-24 09:00:49 +0300

    wrap projects with div

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,42 +1,48 @@
 import React, { Component } from 'react';
-// import './../styles/projects.css';
+import tribute from '../img/tribute.jpg';
+import doc from '../img/doc.jpg';
+import survey from '../img/survey.jpg';
+import land from '../img/land.jpg';
+import './../styles/projects.css';
 
 class Projects extends Component {
   render() {
     return (
       <section id="projects">
-        <h2>These are some of my projects..</h2>
-        <div class="project-tile left-column">
-          <a href="http://sergeialimov.com/projects/tribute-page.html" 
-             target="_blank"
-             rel="noopener noreferrer">
-            <img className="project-pic" src="./../img/tribute.jpg" alt="Tribute page"/>
-            <p className="project-name">Tribute page</p>
-          </a>
-        </div>
-        <div class="project-tile right-column">
-          <a href="http://sergeialimov.com/projects/survey-form.html" 
-             target="_blank"
-             rel="noopener noreferrer">
-            <img class="project-pic" src="./img/survey.jpg" alt="Survey Form"/>
-            <p class="project-name">Survey Form</p>
-          </a>
-        </div>
-        <div class="project-tile left-column">
-          <a href="http://sergeialimov.com/projects/tech-doc.html" 
-          target="_blank"
-          rel="noopener noreferrer">
-            <img class="project-pic" src="./img/doc.jpg" alt="Technical documentation"/>
-            <p class="project-name">Technical documentation</p>
-          </a>
-        </div>
-        <div class="project-tile right-column">
-          <a href="http://sergeialimov.com/projects/product-landing.html" 
-             target="_blank"
-             rel="noopener noreferrer">
-            <img class="project-pic" src="./img/land.jpg" alt="Product landing page"/>
-            <p class="project-name">Product landing page</p>
-          </a>
+        <h2 id="projects-header">These are some of my projects..</h2>
+        <div id="project-tiles">
+          <div class="project-tile left-column">
+            <a href="http://sergeialimov.com/projects/tribute-page.html"
+               target="_blank"
+               rel="noopener noreferrer">
+              <img className="project-pic" src={tribute} alt="Tribute page"/>
+              <p className="project-name">Tribute page</p>
+            </a>
+          </div>
+          <div class="project-tile right-column">
+            <a href="http://sergeialimov.com/projects/survey-form.html"
+               target="_blank"
+               rel="noopener noreferrer">
+              <img class="project-pic" src={survey} alt="Survey Form"/>
+              <p class="project-name">Survey Form</p>
+            </a>
+          </div>
+          <div class="project-tile left-column">
+            <a href="http://sergeialimov.com/projects/tech-doc.html"
+               target="_blank"
+               rel="noopener noreferrer">
+              <img class="project-pic" src={doc} alt="Technical documentation"/>
+              <p class="project-name">Technical documentation</p>
+            </a>
+          </div>
+          <div class="project-tile right-column">
+            <a href="http://sergeialimov.com/projects/product-landing.html"
+               target="_blank"
+               rel="noopener noreferrer">
+              <img class="project-pic" src={land} alt="Product landing page"/>
+              <p class="project-name">Product landing page</p>
+            </a>
+          </div>
         </div>
       </section>
     )
