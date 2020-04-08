dbb7debf418f2fa8d768c98b6e6236d9cf2fb4e2
Sergei Alimov <8912652@gmail.com>
2020-04-08 20:22:28 +0300

    remove unused code

--- push/src/store.js
+++
@@ -1,6 +0,0 @@
-import { reducer } from './reducers/reducer.js';
-import { createStore } from 'redux';
-
-const extenstion = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-
-export default createStore(reducer, extenstion);
