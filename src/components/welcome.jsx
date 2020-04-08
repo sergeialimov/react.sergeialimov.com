3b226d0099fae888bb84577d06d1d411af27de30
Sergei Alimov <8912652@gmail.com>
2020-04-08 20:37:38 +0300

    add test header

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -6,7 +6,8 @@ class Welcome extends Component {
     return (
       <section id="welcome-section">
         <h1>Hey I'am Sergei Alimov</h1>
-        <h2 class="job-position"> Remote Javascript developer</h2>
+        <h2 class="job-position">Remote Javascript developer</h2>
+        <h2 class="job-position">[ test ]</h2>
       </section>  
     )
   }
