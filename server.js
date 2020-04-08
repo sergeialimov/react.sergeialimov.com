139f345d42d7bac535ffa4475733e5d97807bcb2
Sergei Alimov <8912652@gmail.com>
2020-04-08 20:44:06 +0300

    update path in use()

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 
 console.log('server starting')
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/build'));
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './index.html'));
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(port);
