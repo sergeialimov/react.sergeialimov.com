ce0d4aa429c2e0a6ddf797d4950271d4c469cd0c
Sergei Alimov <8912652@gmail.com>
2020-04-30 20:28:17 +0300

    add BaseTemplate

---
+++ src/templates/Base/Base.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Header from '../components/Header.jsx';
+// import './base.styl';
+
+
+const BaseTemplate = (props) => {
+  const {
+    content,
+  } = props;
+  return (
+    <div className="base-template">
+      <Header />
+      <div className="base-template__content">{content}</div>
+    </div>
+  );
+};
+
+BaseTemplate.propTypes = {
+  content: PropTypes.object,
+};
+
+export default BaseTemplate;
