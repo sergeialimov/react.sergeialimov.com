4e18163f5f2fbc1e0dd6d18b23fb1c39218abb20
Sergei Alimov <8912652@gmail.com>
2020-04-30 19:58:11 +0300

    remove unused components

--- src/core/app.jsx
+++ src/core/app.jsx
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
-import Header from '@src/templates/Header.jsx';
 import Welcome from '@src/scenes/Welcome.jsx';
 import Projects from '@src/scenes/projects/Projects.jsx';
 import Social from '@src/scenes/Social.jsx';
-import Footer from '@src/templates/Footer.jsx';
 import '@src/styles/app.css';
 
 class App extends Component {
