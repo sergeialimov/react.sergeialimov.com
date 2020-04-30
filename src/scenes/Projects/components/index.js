969f981070680b8e61d1baaeae5bf7887d482f11
Sergei Alimov <8912652@gmail.com>
2020-04-30 23:25:17 +0300

    update projects components structure

---
+++ src/scenes/Projects/components/index.js
@@ -0,0 +1,4 @@
+export { default as ProductLanding } from './Product-landing';
+export { default as SurveyForm } from './SurveyForm';
+export { default as TechDoc } from './TechDoc';
+export { default as TributePage } from './TributePage';
