ca0e0382c38042a425b74feb774a45979579d68f
Sergei Alimov <8912652@gmail.com>
2020-04-30 20:12:32 +0300

    add Link components to navigation

--- src/templates/components/Header.jsx
+++ src/templates/components/Header.jsx
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-import { NavLink } from 'react-router-dom';
 import '@src/styles/header.css';
+
 
 class Header extends Component {
   render() {
     return (
-      <nav id="navbar">
-        <ul>
-          <li><a className="nav-link" href="#welcome-section">About</a></li>
-          <li><a className="nav-link" href="#projects">Work</a></li>
-          <li><a className="nav-link" href="#social">Contact</a></li>
-        </ul>
-  );
-      </nav>
-    )
+      <div>
+        <nav id="navbar">
+          <ul>
+            <li>
+              <Link className="nav-link" to="/">About</Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/social">Contacts</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
   }
 }
 
