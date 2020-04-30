5c2a64733955252b79aa78a6a5a37b56ccefaa84
Sergei Alimov <8912652@gmail.com>
2020-04-30 19:37:41 +0300

    capitalize components files

--- src/core/app.jsx
+++ src/core/app.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
-import Header from '@src/templates/header.jsx';
-import Welcome from '@src/scenes/welcome.jsx';
-import Projects from '@src/scenes/projects/projects.jsx';
-import Social from '@src/scenes/social.jsx';
-import Footer from '@src/templates/footer.jsx';
+import Header from '@src/templates/Header.jsx';
+import Welcome from '@src/scenes/Welcome.jsx';
+import Projects from '@src/scenes/projects/Projects.jsx';
+import Social from '@src/scenes/Social.jsx';
+import Footer from '@src/templates/Footer.jsx';
 import '@src/styles/app.css';
 
 class App extends Component {
