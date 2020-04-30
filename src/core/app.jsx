1217401ff23ba5077d046ee958ea6cc38b21af65
Sergei Alimov <8912652@gmail.com>
2020-04-30 19:41:09 +0300

    add routing

--- src/core/app.jsx
+++ src/core/app.jsx
@@ -9,21 +9,61 @@ import Social from '@src/scenes/Social.jsx';
 import Footer from '@src/templates/Footer.jsx';
 import '@src/styles/app.css';
 
-class App extends Component {
-  componentDidMount() {
-    document.body.style.margin = '0';
-  }
-  render() {
-    return (
-      <div id="app">
-        <Header/>
-        <Welcome/>
-        <Projects/>
-        <Social/>
-        <Footer/>
-      </div>
-    )
-  }
-}
+// class App extends Component {
+//   componentDidMount() {
+//     document.body.style.margin = '0';
+//   }
+//   render() {
+//     return (
+//       <div id="app">
+//         <Header/>
+//         <Welcome/>
+//         <Projects/>
+//         <Social/>
+//         <Footer/>
+//       </div>
+//     )
+//   }
+// }
 
-export default App;
+// export default App;
+
+
+
+
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Welcome</Link>
+            </li>
+            <li>
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link to="/social">Social</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/social">
+            <Social />
+          </Route>
+          <Route path="/">
+            <Welcome />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
