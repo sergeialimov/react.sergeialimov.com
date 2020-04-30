dcf1dee839e94f77cd63f1b0846c5449899edba0
Sergei Alimov <8912652@gmail.com>
2020-04-30 19:27:21 +0300

    fix path errors

--- src/core/app.jsx
+++ src/core/app.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
-import Header from './header.jsx';
-import Welcome from './welcome.jsx';
-import Projects from './projects/projects.jsx';
-import Social from './social.jsx';
-import Footer from './footer.jsx';
+import Header from '@src/templates/header.jsx';
+import Welcome from '@src/scenes/welcome.jsx';
+import Projects from '@src/scenes/projects/projects.jsx';
+import Social from '@src/scenes/social.jsx';
+import Footer from '@src/templates/footer.jsx';
 import '@src/styles/app.css';
 
 class App extends Component {
