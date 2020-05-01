51eca9df64f0f68f435adf022cd5724ebe91fd27
Sergei Alimov <8912652@gmail.com>
2020-05-01 09:58:19 +0300

    update h1 text

--- src/scenes/About/About.jsx
+++ src/scenes/About/About.jsx
@@ -5,7 +5,7 @@ import BaseTemplate from '@src/templates/Base';
 const About = (props) => {
   const content = (
     <section id="welcome-section">
-      <h1>Hey I'am Sergei Alimov</h1>
+      <h1>Hi, I'am Sergei Alimov</h1>
       <h2 className="job-position">Remote Javascript developer</h2>
     </section>
   );
