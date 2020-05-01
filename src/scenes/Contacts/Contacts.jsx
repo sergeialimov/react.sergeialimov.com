61dcc4aa7ed99fa238dff65c32df3dcf8dd0985b
Sergei Alimov <8912652@gmail.com>
2020-05-01 09:33:18 +0300

    integrate Contact component

--- src/scenes/Contacts/Contacts.jsx
+++ src/scenes/Contacts/Contacts.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import BaseTemplate from '@src/templates/Base';
+import Contact from './components';
+
 import './contacts.css';
 
 const Contacts = (props) => {
@@ -8,54 +10,24 @@ const Contacts = (props) => {
     <section id="social">
       <header>
         <h3>Let's work together</h3>
-        <p className="coffee">How do you take your coffee?</p>
       </header>
       <div className="contacts">
-        <a
-          id="profile-link"
-          href="https://github.com/sergeialimov"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="contact">
-            Github
-          </div>
-        </a>
-        <a
-          id="profile-link"
-          href="https://www.linkedin.com/in/sergei-alimov/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="contact">
-            LinkedIn
-          </div>
-        </a>
-        <a
-          id="profile-link"
-          href="https://www.facebook.com/alimov.s"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="contact">
-            Facebook
-          </div>
-        </a>
-        <a
-          id="profile-link"
-          href="mailto:8912652@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="contact">
-            Email
-          </div>
-        </a>
-        <a id="profile-link" href="tel:+79313392819" target="_blank" rel="noopener noreferrer">
-          <div className="contact">
-            Call me
-          </div>
-        </a>
+        <Contact
+          link="https://github.com/sergeialimov"
+          content="Github"
+        />
+        <Contact
+          link="https://www.linkedin.com/in/sergei-alimov/"
+          content="LinkedIn"
+        />
+        <Contact
+          link="https://t.me/sergeialimov"
+          content="Telegram"
+        />
+        <Contact
+          link="mailto:8912652@gmail.com"
+          content="Email"
+        />
       </div>
     </section>
   );
