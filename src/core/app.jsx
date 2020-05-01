bcfbda5f1833f6f826f08a918f5e4977d1b68efa
Sergei Alimov <8912652@gmail.com>
2020-05-01 08:57:57 +0300

    wrap App with div

--- src/core/app.jsx
+++ src/core/app.jsx
@@ -13,33 +13,35 @@ class App extends Component {
   }
   render() {
     return (
-      <Router>
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/contacts">
-            <Contacts />
-          </Route>
-          <Route path="/projects/product-landing">
-            <h2>product-landing</h2>;
-          </Route>
-          <Route path="/projects/survey-form">
-            <h2>survey-form</h2>;
-          </Route>
-          <Route path="/projects/tech-doc">
-            <h2>tech-doc</h2>;
-          </Route>
-          <Route path="/projects/tribute-page">
-            <h2>tribute-page</h2>;
-          </Route>
-          <Route path="/projects">
-            <Projects />
-          </Route>
-          <Route path="/">
-            <About />
-          </Route>
-        </Switch>
-    </Router>
+      <div id="app">
+        <Router>
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/contacts">
+              <Contacts />
+            </Route>
+            <Route path="/projects/product-landing">
+              <h2>product-landing</h2>;
+            </Route>
+            <Route path="/projects/survey-form">
+              <h2>survey-form</h2>;
+            </Route>
+            <Route path="/projects/tech-doc">
+              <h2>tech-doc</h2>;
+            </Route>
+            <Route path="/projects/tribute-page">
+              <h2>tribute-page</h2>;
+            </Route>
+            <Route path="/projects">
+              <Projects />
+            </Route>
+            <Route path="/">
+              <About />
+            </Route>
+          </Switch>
+      </Router>
+    </div>
     )
   }
 }
