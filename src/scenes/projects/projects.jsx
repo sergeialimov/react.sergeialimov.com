4b72f689c41e8c807d8f70d8f8ec57a2c101a794
Sergei Alimov <8912652@gmail.com>
2020-05-01 13:45:20 +0300

    apply project component to projects

--- src/scenes/projects/projects.jsx
+++ src/scenes/projects/projects.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 import BaseTemplate from '@src/templates/Base';
+import Project from './components';
 
 import tribute from './components/Tribute-page/tribute.jpg';
 import doc from './components/Tech-doc/doc.jpg';
 import survey from './components/Survey-form/survey.jpg';
 import land from './components/Product-landing/land.jpg';
+
 import './projects.css';
 
 
@@ -14,38 +16,30 @@ const Projects = (props) => {
     <section id="projects">
       <h1 className="projects__header-h1">These are some of my projects..</h1>
       <div id="project-tiles">
-        <div className="project-tile left-column">
-          <a href="http://sergeialimov.com/projects/tribute-page.html"
-              target="_blank"
-              rel="noopener noreferrer">
-            <img className="project-pic" src={tribute} alt="Tribute page"/>
-            <p className="project-name">Tribute page</p>
-          </a>
-        </div>
-        <div className="project-tile right-column">
-          <a href="http://sergeialimov.com/projects/survey-form.html"
-              target="_blank"
-              rel="noopener noreferrer">
-            <img className="project-pic" src={survey} alt="Survey Form"/>
-            <p className="project-name">Survey Form</p>
-          </a>
-        </div>
-        <div className="project-tile left-column">
-          <a href="http://sergeialimov.com/projects/tech-doc.html"
-              target="_blank"
-              rel="noopener noreferrer">
-            <img className="project-pic" src={doc} alt="Technical documentation"/>
-            <p className="project-name">Technical documentation</p>
-          </a>
-        </div>
-        <div className="project-tile right-column">
-          <a href="http://sergeialimov.com/projects/product-landing.html"
-              target="_blank"
-              rel="noopener noreferrer">
-            <img className="project-pic" src={land} alt="Product landing page"/>
-            <p className="project-name">Product landing page</p>
-          </a>
-        </div>
+        <Project
+          content="Tribute page"
+          classNm="project-tile left-column"
+          path="/projects/tribute-page"
+          src={tribute}
+        />
+        <Project
+          content="Survey Form"
+          classNm="project-tile right-column"
+          path="/projects/survey-form"
+          src={survey}
+        />
+        <Project
+          content="Technical documentation"
+          classNm="project-tile left-column"
+          path="/projects/tech-doc"
+          src={doc}
+        />
+        <Project
+          content="Product landing page"
+          classNm="project-tile right-column"
+          path="/projects/product-landing"
+          src={land}
+        />
       </div>
     </section>
   );
