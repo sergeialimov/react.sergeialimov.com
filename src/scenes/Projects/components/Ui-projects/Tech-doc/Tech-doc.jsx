6bf86a9788dd6961a7acc96c16226642b2684292
Sergei Alimov <8912652@gmail.com>
2020-05-05 17:47:16 +0300

    added styles for techDoc

--- src/scenes/Projects/components/Ui-projects/Tech-doc/Tech-doc.jsx
+++ src/scenes/Projects/components/Ui-projects/Tech-doc/Tech-doc.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 import Navigation from './components/Navigation';
 import PageData from './components/PageData';
 
+import './techDoc.css';
 
 const TechDoc = () => {
   return (
-    <main className="tech-doc__main" id="main-doc">
+    <main className="tech-doc__main">
       <Navigation />
-      {/* <PageData /> */}
+      <PageData />
     </main>
   );
 }
