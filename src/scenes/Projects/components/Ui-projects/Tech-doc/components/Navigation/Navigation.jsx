c83f6dd32dd0a633e65b30f7a2bc56d2a1bf31f0
Sergei Alimov <8912652@gmail.com>
2020-05-05 17:40:44 +0300

    update classNames in navigation

--- src/scenes/Projects/components/Ui-projects/Tech-doc/components/Navigation/Navigation.jsx
+++ src/scenes/Projects/components/Ui-projects/Tech-doc/components/Navigation/Navigation.jsx
@@ -5,18 +5,18 @@ import './navigation.css';
 
 const Navigation = () => {
   return (
-    <nav className="tech-doc__navbar">
+    <nav className="tech-doc__navigation">
       <header>
-        <h1 className="tech-doc__header__h1">JS Documentation</h1>
+        <h1 className="tech-doc__navigation__header__h1">JS Documentation</h1>
       </header>
-      <ul className="tech-doc__nav-list">
-        <li><a className="tech-doc__nav-link" href="#Introduction">Introduction</a></li>
-        <li><a className="tech-doc__nav-link" href="#What_you_should_already_know">What you should already know</a></li>
-        <li><a className="tech-doc__nav-link" href="#Javascript_and_Java">JavaScript and Java</a></li>
-        <li><a className="tech-doc__nav-link" href="#Hello_world">Hello world</a></li>
-        <li><a className="tech-doc__nav-link" href="#Variables">Variables</a></li>
-        <li><a className="tech-doc__nav-link" href="#Declaring_variables">Declaring variables</a></li>
-        <li><a className="tech-doc__nav-link" href="#Variable_scope">Variable scope</a></li>
+      <ul className="tech-doc__navigation__list">
+        <li><a className="tech-doc__navigation__link" href="#Introduction">Introduction</a></li>
+        <li><a className="tech-doc__navigation__link" href="#What_you_should_already_know">What you should already know</a></li>
+        <li><a className="tech-doc__navigation__link" href="#Javascript_and_Java">JavaScript and Java</a></li>
+        <li><a className="tech-doc__navigation__link" href="#Hello_world">Hello world</a></li>
+        <li><a className="tech-doc__navigation__link" href="#Variables">Variables</a></li>
+        <li><a className="tech-doc__navigation__link" href="#Declaring_variables">Declaring variables</a></li>
+        <li><a className="tech-doc__navigation__link" href="#Variable_scope">Variable scope</a></li>
       </ul>
     </nav>
   );
