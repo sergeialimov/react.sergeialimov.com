fffe3dbabb6a87dd3f7bc517afd6007e218d7888
Sergei Alimov <8912652@gmail.com>
2020-05-05 18:23:44 +0300

    render data with map()

--- src/scenes/Projects/components/Ui-projects/Tech-doc/components/PageData/PageData.jsx
+++ src/scenes/Projects/components/Ui-projects/Tech-doc/components/PageData/PageData.jsx
@@ -1,31 +1,28 @@
-
 import React from 'react';
 
-import varName from './pageDataContent.js';
+import pageDataContent from './pageDataContent.js';
 
 import './pageData.css';
+
+const keys = Object.keys(pageDataContent);
+
+const content = keys.map(
+  key => (
+    <section className="main-section" id={pageDataContent[key].id}>
+      <header>
+        <h2 className="tech-doc__page-data__header_h2">{pageDataContent[key].header}</h2>
+      </header>
+      {pageDataContent[key].body}
+    </section>
+  )
+);
 
 const PageData = () => {
   return (
     <div>
-      {varName.key}
+      {content}
     </div>
   );
 }
 
 export default PageData;
-
-
-
-// const keys = Object.entries(content);
-  // const techDocContent = keys.map = (x) => {
-  //   x = (
-  //     <section className="main-section" id={content[x].header}>
-  //       <header>
-  //         <h2 className="tech-doc__header__h2">content[x].header</h2>
-  //       </header>
-  //       {content[x].text}
-  //     </section>
-  //   )
-  // };
-  // console.log('-------------------', data)
