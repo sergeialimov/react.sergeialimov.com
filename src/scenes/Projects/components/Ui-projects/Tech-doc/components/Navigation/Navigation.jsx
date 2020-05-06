37255d6efebf47b7af62d5d0596a3b97d32127e2
Sergei Alimov <8912652@gmail.com>
2020-05-06 12:24:22 +0300

    update anchor links

--- src/scenes/Projects/components/Ui-projects/Tech-doc/components/Navigation/Navigation.jsx
+++ src/scenes/Projects/components/Ui-projects/Tech-doc/components/Navigation/Navigation.jsx
@@ -10,13 +10,13 @@ const Navigation = () => {
         <h1 className="tech-doc__navigation__header__h1">JS Documentation</h1>
       </header>
       <ul className="tech-doc__navigation__list">
-        <li><a className="tech-doc__navigation__link" href="#Introduction">Introduction</a></li>
-        <li><a className="tech-doc__navigation__link" href="#What_you_should_already_know">What you should already know</a></li>
-        <li><a className="tech-doc__navigation__link" href="#Javascript_and_Java">JavaScript and Java</a></li>
-        <li><a className="tech-doc__navigation__link" href="#Hello_world">Hello world</a></li>
-        <li><a className="tech-doc__navigation__link" href="#Variables">Variables</a></li>
-        <li><a className="tech-doc__navigation__link" href="#Declaring_variables">Declaring variables</a></li>
-        <li><a className="tech-doc__navigation__link" href="#Variable_scope">Variable scope</a></li>
+        <li><a className="tech-doc__navigation__link" href="#introduction">Introduction</a></li>
+        <li><a className="tech-doc__navigation__link" href="#requirements">What you should already know</a></li>
+        <li><a className="tech-doc__navigation__link" href="#jsAndJava">JavaScript and Java</a></li>
+        <li><a className="tech-doc__navigation__link" href="#helloWorld">Hello world</a></li>
+        <li><a className="tech-doc__navigation__link" href="#variables">Variables</a></li>
+        <li><a className="tech-doc__navigation__link" href="#declaringVariables">Declaring variables</a></li>
+        <li><a className="tech-doc__navigation__link" href="#variableScope">Variable scope</a></li>
       </ul>
     </nav>
   );
