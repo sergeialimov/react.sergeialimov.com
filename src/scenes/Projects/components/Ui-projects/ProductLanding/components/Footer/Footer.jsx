b1465ffa9273556ff8ddec9c86fced2028e67626
Sergei Alimov <8912652@gmail.com>
2020-06-19 20:59:32 +0300

    replace a with p

--- src/scenes/Projects/components/Ui-projects/ProductLanding/components/Footer/Footer.jsx
+++ src/scenes/Projects/components/Ui-projects/ProductLanding/components/Footer/Footer.jsx
@@ -4,9 +4,9 @@ const Footer = () => (
   <footer>
     <div className="footer-links">
       <ul>
-        <li><a className="footer-link" href="#">Privacy</a></li>
-        <li><a className="footer-link" href="#">Terms</a></li>
-        <li><a className="footer-link" href="#">Contact</a></li>
+        <li><p className="footer-link">Privacy</p></li>
+        <li><p className="footer-link">Terms</p></li>
+        <li><p className="footer-link">Contact</p></li>
       </ul>
     </div>
     <div className="copyright">Copyright 2016, Original Trombones</div>
