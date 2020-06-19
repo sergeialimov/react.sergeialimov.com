78efa15a1b8a4ab09ee76f5082beabc99dea132d
Sergei Alimov <8912652@gmail.com>
2020-06-19 19:01:13 +0300

    update classnames

--- src/scenes/Projects/components/Ui-projects/SurveyForm/components/Recommendation/Recommendation.jsx
+++ src/scenes/Projects/components/Ui-projects/SurveyForm/components/Recommendation/Recommendation.jsx
@@ -3,22 +3,22 @@ import React from 'react';
 import './recommendation.css';
 
 const Recommendation = () => (
-  <div className="radio-group">
-    <div className="radio-item">
-      <label className="radio-label" htmlFor="definitely">
-        <input className="radio-input" id="definitely" type="radio" name="recommendation" value="1" />
+  <div className="recommendation__radio-group">
+    <div className="recommendation__radio-item">
+      <input className="recommendation__radio-input" id="definitely" type="radio" name="recommendation" value="1" />
+      <label className="recommendation__radio-label" htmlFor="definitely">
         Definitely
       </label>
     </div>
-    <div className="radio-item">
-      <label className="radio-label" htmlFor="maybe">
-        <input className="radio-input" id="maybe" type="radio" name="recommendation" value="2" />
+    <div className="recommendation__radio-item">
+      <input className="recommendation__radio-input" id="maybe" type="radio" name="recommendation" value="2" />
+      <label className="recommendation__radio-label" htmlFor="maybe">
         May be
       </label>
     </div>
-    <div className="radio-item">
-      <label className="radio-label" htmlFor="notsure">
-        <input className="radio-input" id="notSure" type="radio" name="recommendation" value="3" />
+    <div className="recommendation__radio-item">
+      <input className="recommendation__radio-input" id="notSure" type="radio" name="recommendation" value="3" />
+      <label className="recommendation__radio-label" htmlFor="notsure">
         Not sure
       </label>
     </div>
