b49a98695c55b9271e048cd448197f03b4251119
Sergei Alimov <8912652@gmail.com>
2020-06-21 17:54:33 +0300

    move out pricing css

--- src/scenes/Projects/components/Ui-projects/ProductLanding/components/Main/components/Pricing/Pricing.jsx
+++ src/scenes/Projects/components/Ui-projects/ProductLanding/components/Main/components/Pricing/Pricing.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 
+import './pricing.css';
+
 const Pricing = () => (
-  <div id="pricing">
-    <div className="price-block">
-      <div className="price-header">TENOR TROMBONE</div>
-      <p className="price">$600</p>
-      <p className="description">{'Lorem ipsum.\nLorem ipsum.\nLorem ipsum dolor.\nLorem ipsum'}</p>
-      <div className="select-wrap">
-        <input className="select" type="submit" name="select" id="select-tenor" value="SELECT" />
+  <div className="pricing__container">
+    <div className="pricing__price-block">
+      <div className="pricing__price__header">TENOR TROMBONE</div>
+      <p className="pricing__price">$600</p>
+      <p className=".pricing__description">{'Lorem ipsum.\nLorem ipsum.\nLorem ipsum dolor.\nLorem ipsum'}</p>
+      <div className="pricing__submit__wrapper">
+        <input className="pricing__submit__button" type="submit" name="select" id="select-tenor" value="SELECT" />
       </div>
     </div>
-    <div className="price-block">
-      <div className="price-header">BASS TROMBONE</div>
-      <p className="price">$900</p>
-      <p className="description">{'Lorem ipsum.\nLorem ipsum.\nLorem ipsum dolor.\nLorem ipsum.'}</p>
-      <div className="select-wrap">
-        <input className="select" type="submit" name="select" id="select-bass" value="SELECT" />
+    <div className="pricing__price-block">
+      <div className="pricing__price__header">BASS TROMBONE</div>
+      <p className="pricing__price">$900</p>
+      <p className="pricing__description">{'Lorem ipsum.\nLorem ipsum.\nLorem ipsum dolor.\nLorem ipsum.'}</p>
+      <div className="pricing__submit__wrapper">
+        <input className="pricing__submit__button" type="submit" name="select" id="select-bass" value="SELECT" />
       </div>
     </div>
-    <div className="price-block">
-      <div className="price-header">VALVE TROMBONE</div>
-      <p className="price">$1200</p>
-      <p className="description">{'Lorem ipsum.\nLorem ipsum.\nLorem ipsum dolor.\nLorem ipsum.'}</p>
-      <div className="select-wrap">
-        <input className="select" type="submit" name="select" id="select-valve" value="SELECT" />
+    <div className="pricing__price-block">
+      <div className="pricing__price__header">VALVE TROMBONE</div>
+      <p className="pricing__price">$1200</p>
+      <p className="pricing__description">{'Lorem ipsum.\nLorem ipsum.\nLorem ipsum dolor.\nLorem ipsum.'}</p>
+      <div className="pricing__submit__wrapper">
+        <input className="pricing__submit__button" type="submit" name="select" id="select-valve" value="SELECT" />
       </div>
     </div>
   </div>
