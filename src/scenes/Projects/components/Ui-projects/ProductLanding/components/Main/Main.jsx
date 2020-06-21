bcccfbd002a223abe6de0571c804cb7add95c1d9
Sergei Alimov <8912652@gmail.com>
2020-06-21 18:31:09 +0300

    move out howItWorks

--- src/scenes/Projects/components/Ui-projects/ProductLanding/components/Main/Main.jsx
+++ src/scenes/Projects/components/Ui-projects/ProductLanding/components/Main/Main.jsx
@@ -8,9 +8,7 @@ const Main = () => (
     <h1 className="product-landing__header__h1">Handcrafted, home-made masterpieces</h1>
     <GetStarted />
     <Features />
-    <div className="product-landing__how-it-works">
-      <iframe id="video" width="560" height="315" src="https://www.youtube.com/embed/y8Yv4pnO7qc" frameBorder="0" allowFullScreen title="how-it-works" />
-    </div>
+    <HoItWorks />
     <Pricing />
   </main>
 );
