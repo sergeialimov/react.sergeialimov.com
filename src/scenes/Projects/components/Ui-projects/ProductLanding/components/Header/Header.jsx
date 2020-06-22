2aaa589854a4433fc2d9d9f190e94c0de3bd3405
Sergei Alimov <8912652@gmail.com>
2020-06-22 11:50:27 +0300

    fix display styles

--- src/scenes/Projects/components/Ui-projects/ProductLanding/components/Header/Header.jsx
+++ src/scenes/Projects/components/Ui-projects/ProductLanding/components/Header/Header.jsx
@@ -7,9 +7,9 @@ const Header = () => (
     <img className="product-landing__header__logo" src="https://s3.amazonaws.com/freecodecamp/original_trombones.png" alt="trombones" />
     <nav className="product-landing__header__navigation">
       <ul className="product-landing__header__navigation__ul">
-        <li><a className=".product-landing__header__navigation__li" href="#feature">Features</a></li>
-        <li><a className=".product-landing__header__navigation__li" href="#how-it-works">How it works</a></li>
-        <li><a className=".product-landing__header__navigation__li" href="#pricing">Pricing</a></li>
+        <li><a className="product-landing__header__navigation__li" href="#feature">Features</a></li>
+        <li><a className="product-landing__header__navigation__li" href="#how-it-works">How it works</a></li>
+        <li><a className="product-landing__header__navigation__li" href="#pricing">Pricing</a></li>
       </ul>
     </nav>
   </header>
