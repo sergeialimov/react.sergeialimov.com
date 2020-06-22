34ff86070793fd0b878f2fbf29fca8b6fa6c9261
Sergei Alimov <8912652@gmail.com>
2020-06-22 12:31:00 +0300

    add real content

--- src/scenes/Projects/components/Ui-projects/ProductLanding/components/Main/components/Pricing/Pricing.jsx
+++ src/scenes/Projects/components/Ui-projects/ProductLanding/components/Main/components/Pricing/Pricing.jsx
@@ -7,7 +7,13 @@ const Pricing = () => (
     <div className="pricing__price-block">
       <div className="pricing__price__header">TENOR TROMBONE</div>
       <p className="pricing__price">$600</p>
-      <p className=".pricing__description">{'Lorem ipsum.\nLorem ipsum.\nLorem ipsum dolor.\nLorem ipsum'}</p>
+      <p className="pricing__description">
+        {
+        `The JP135 is John Packer’s Bb valve trombone.
+        Ideal for valve players who want to dabble with a trombone without learning slide positions.
+        Also very handy for trumpet teachers to take to trombone lessons to help the student learn and focus on the sound.`
+        }
+      </p>
       <div className="pricing__submit__wrapper">
         <input className="pricing__submit__button" type="submit" name="select" id="select-tenor" value="SELECT" />
       </div>
@@ -15,7 +21,13 @@ const Pricing = () => (
     <div className="pricing__price-block">
       <div className="pricing__price__header">BASS TROMBONE</div>
       <p className="pricing__price">$900</p>
-      <p className="pricing__description">{'Lorem ipsum.\nLorem ipsum.\nLorem ipsum dolor.\nLorem ipsum.'}</p>
+      <p className="pricing__description">
+        {
+        `The JP230 Rath would be suitable for a progressing player and would be suited to playing in wind bands and concert bands, as well as performing in jazz ensembles or big bands.
+        They are characterised by a professional standard of workmanship and a very high level of performance.
+        `
+        }
+      </p>
       <div className="pricing__submit__wrapper">
         <input className="pricing__submit__button" type="submit" name="select" id="select-bass" value="SELECT" />
       </div>
@@ -23,7 +35,13 @@ const Pricing = () => (
     <div className="pricing__price-block">
       <div className="pricing__price__header">VALVE TROMBONE</div>
       <p className="pricing__price">$1200</p>
-      <p className="pricing__description">{'Lorem ipsum.\nLorem ipsum.\nLorem ipsum dolor.\nLorem ipsum.'}</p>
+      <p className="pricing__description">
+        {
+          `The JP333 Rath is John Packer's Double Valve Bass Trombone and is suitable for musicians who are looking for a mid range/pro level instrument perfectly suited to orchestral playing,
+          in a brass band section and also in a trombone section in a big band.
+          `
+        }
+      </p>
       <div className="pricing__submit__wrapper">
         <input className="pricing__submit__button" type="submit" name="select" id="select-valve" value="SELECT" />
       </div>
