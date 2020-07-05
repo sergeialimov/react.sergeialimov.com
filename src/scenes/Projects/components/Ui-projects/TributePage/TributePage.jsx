abf47fae8a693295c463ad2cb749a8f6d2496c12
Sergei Alimov <8912652@gmail.com>
2020-07-05 12:45:59 +0300

    fix formatting in TributePage

--- src/scenes/Projects/components/Ui-projects/TributePage/TributePage.jsx
+++ src/scenes/Projects/components/Ui-projects/TributePage/TributePage.jsx
@@ -9,12 +9,12 @@ const TributePage = () => (
       <p>The man who saved a billion lives</p>
     </div>
     <div className="tribute-page__image-wrapper">
-      <img className="tribute-page__image" src="https://c2.staticflickr.com/4/3689/10613180113_fdf7bcd316_b.jpg" />
+      <img className="tribute-page__image" src="https://c2.staticflickr.com/4/3689/10613180113_fdf7bcd316_b.jpg" alt="tribute page" />
       <p className="tribute-page__image-caption">Dr. Norman Borlaug, second from left, trains biologists in Mexico on how to increase wheat yields - part of his life-long war on hunger.</p>
     </div>
     <div className="tribute-page__biography">
       <h2 className="tribute-page__header__h2">
-        Here's a time line of Dr. Borlaug's life:
+        Here&apos;s a time line of Dr. Borlaug&apos;s life:
       </h2>
       <ul className="tribute-page__list">
         <li className="tribute-page__list-item">
@@ -25,12 +25,19 @@ const TributePage = () => (
         <li className="tribute-page__list-item">
           <strong>1933</strong>
           {' '}
-          - Leaves his family's farm to attend the University of Minnesota, thanks to a Depression era program known as the "National Youth Administration"
+          {
+          `- Leaves his family's farm to attend the University of Minnesota, thanks to a Depression
+          era program known as the "National Youth Administration"`
+          }
         </li>
         <li className="tribute-page__list-item">
           <strong>1935</strong>
           {' '}
-          - Has to stop school and save up more money. Works in the Civilian Conservation Corps, helping starving Americans. "I saw how food changed them", he said. "All of this left scars on me."
+          {
+          `- Has to stop school and save up more money. Works in the Civilian Conservation Corps,
+          helping starving Americans. "I saw how food changed them", he said. "All of this left
+          scars on me."`
+          }
         </li>
         <li className="tribute-page__list-item">
           <strong>1937</strong>
@@ -40,12 +47,20 @@ const TributePage = () => (
         <li className="tribute-page__list-item">
           <strong>1938</strong>
           {' '}
-          - Marries wife of 69 years Margret Gibson. Gets laid off due to budget cuts. Inspired by Elvin Charles Stakman, he returns to school study under Stakman, who teaches him about breeding pest-resistent plants.
+          {
+          `- Marries wife of 69 years Margret Gibson. Gets laid off due to budget cuts. Inspired by
+          Elvin Charles Stakman, he returns to school study under Stakman, who teaches him about
+          breeding pest-resistent plants.`
+          }
         </li>
         <li className="tribute-page__list-item">
           <strong>1941</strong>
           {' '}
-          - Tries to enroll in the military after the Pearl Harbor attack, but is rejected. Instead, the military asked his lab to work on waterproof glue, DDT to control malaria, disenfectants, and other applied science.
+          {
+          `- Tries to enroll in the military after the Pearl Harbor attack, but is rejected.
+          Instead, the military asked his lab to work on waterproof glue, DDT to control malaria,
+          disenfectants, and other applied science.`
+          }
         </li>
         <li className="tribute-page__list-item">
           <strong>1942</strong>
@@ -55,7 +70,12 @@ const TributePage = () => (
         <li className="tribute-page__list-item">
           <strong>1944</strong>
           {' '}
-          - Rejects a 100% salary increase from Dupont, leaves behind his pregnant wife, and flies to Mexico to head a new plant pathology program. Over the next 16 years, his team breeds 6,000 different strains of disease resistent wheat - including different varieties for each major climate on Earth.
+          {
+          `- Rejects a 100% salary increase from Dupont, leaves behind his pregnant wife, and flies
+          to Mexico to head a new plant pathology program. Over the next 16 years, his team breeds
+          6,000 different strains of disease resistent wheat - including different varieties for
+          each major climate on Earth.`
+          }
         </li>
         <li className="tribute-page__list-item">
           <strong>1945</strong>
@@ -65,12 +85,19 @@ const TributePage = () => (
         <li className="tribute-page__list-item">
           <strong>1953</strong>
           {' '}
-          - crosses a short, sturdy dwarf breed of wheat with a high-yeidling American breed, creating a strain that responds well to fertalizer. It goes on to provide 95% of Mexico's wheat.
+          {
+          `- crosses a short, sturdy dwarf breed of wheat with a high-yeidling American breed,
+          creating a strain that responds well to fertalizer. It goes on to provide 95% of Mexico's
+          wheat.`
+          }
         </li>
         <li className="tribute-page__list-item">
           <strong>1962</strong>
           {' '}
-          - Visits Delhi and brings his high-yielding strains of wheat to the Indian subcontinent in time to help mitigate mass starvation due to a rapidly expanding population
+          {
+          `- Visits Delhi and brings his high-yielding strains of wheat to the Indian subcontinent
+          in time to help mitigate mass starvation due to a rapidly expanding population`
+          }
         </li>
         <li className="tribute-page__list-item">
           <strong>1970</strong>
@@ -90,7 +117,13 @@ const TributePage = () => (
         <li className="tribute-page__list-item">
           <strong>2005</strong>
           {' '}
-          - states "we will have to double the world food supply by 2050." Argues that genetically modified crops are the only way we can meet the demand, as we run out of arable land. Says that GM crops are not inherently dangerous because "we've been genetically modifying plants and animals for a long time. Long before we called it science, people were selecting the best breeds."
+          {
+          `- states "we will have to double the world food supply by 2050." Argues that genetically
+          modified crops are the only way we can meet the demand, as we run out of arable land. Says
+          that GM crops are not inherently dangerous because "we've been genetically modifying
+          plants and animals for a long time. Long before we called it science, people were
+          selecting the best breeds."`
+          }
         </li>
         <li className="tribute-page__list-item">
           <strong>2009</strong>
@@ -102,7 +135,11 @@ const TributePage = () => (
     <div className="tribute-page__quote">
       <p>
         <i>
-          "Borlaug's life and achievement are testimony to the far-reaching contribution that one man's towering intellect, persistence and scientific vision can make to human peace and progress."
+          {
+          `"Borlaug's life and achievement are testimony to the far-reaching contribution that one
+          man's towering intellect, persistence and scientific vision can make to human peace and
+          progress."`
+          }
         </i>
       </p>
       <p><i>-- Indian Prime Minister Manmohan Singh</i></p>
@@ -114,6 +151,7 @@ const TributePage = () => (
           <a
             href="https://en.wikipedia.org/wiki/Norman_Borlaug"
             target="_blank"
+            rel="noreferrer"
             alt=""
           >
             Wikipedia entry
