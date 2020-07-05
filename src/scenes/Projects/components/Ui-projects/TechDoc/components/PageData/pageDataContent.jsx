a5e9c9cee758a38f2351049008f5f6b80b61a57f
Sergei Alimov <8912652@gmail.com>
2020-07-05 12:33:20 +0300

    fix formatting in pageDataContent.jsx

--- src/scenes/Projects/components/Ui-projects/TechDoc/components/PageData/pageDataContent.jsx
+++ src/scenes/Projects/components/Ui-projects/TechDoc/components/PageData/pageDataContent.jsx
@@ -62,19 +62,24 @@ const getPageDataContent = (classNames) => [
     body: (
       <div className={classNames.text}>
         <p className={classNames.p}>
-          JavaScript and Java are similar in some ways but fundamentally different in some others.
+          {
+        `JavaScript and Java are similar in some ways but fundamentally different in some others.
           The JavaScript language resembles Java but does not have Java's static typing and strong
           type checking. JavaScript follows most Java expression syntax, naming conventions and
           basic control-flow constructs which was the reason why it was renamed from LiveScript to
-          JavaScript.
+          JavaScript.`
+        }
         </p>
-        <p className={classNames.p}>In contrast to Java's compile-time system of classes built by
+        <p className={classNames.p}>
+          {
+          `In contrast to Java's compile-time system of classes built by
           declarations, JavaScript supports a runtime system based on a small number of data types
           representing numeric, Boolean, and string values. JavaScript has a prototype-based object
           model instead of the more common class-based object model. The prototype-based model
           provides dynamic inheritance; that is, what is inherited can vary for individual objects.
           JavaScript also supports functions without any special declarative requirements. Functions
-          can be properties of objects, executing as loosely typed methods.
+          can be properties of objects, executing as loosely typed methods.`
+          }
         </p>
         <p className={classNames.p}>
           JavaScript is a very free-form language compared to Java. You do not have to declare all
@@ -91,8 +96,10 @@ const getPageDataContent = (classNames) => [
     body: (
       <div className={classNames.text}>
         <p className={classNames.p}>
-          To get started with writing JavaScript, open the Scratchpad and write your first "Hello
-          world" JavaScript code:
+          {
+          `To get started with writing JavaScript, open the Scratchpad and write your first "Hello
+          world" JavaScript code:`
+        }
         </p>
         <pre className={classNames.code}>
           {
@@ -112,10 +119,12 @@ const getPageDataContent = (classNames) => [
           variables, called identifiers, conform to certain rules.
         </p>
         <p className={classNames.p}>
-          A JavaScript identifier must start with a letter, underscore (_), or dollar sign ($);
+          {
+          `A JavaScript identifier must start with a letter, underscore (_), or dollar sign ($);
           subsequent characters can also be digits (0-9). Because JavaScript is case sensitive,
           letters include the characters "A" through "Z" (uppercase) and the characters "a" through
-          "z" (lowercase).
+          "z" (lowercase).`
+          }
         </p>
         <p className={classNames.p}>
           You can use ISO 8859-1 or Unicode letters such as å and ü in identifiers. You can also use
@@ -139,8 +148,10 @@ const getPageDataContent = (classNames) => [
         <p className={classNames.p}>By simply assigning it a value. For example,</p>
         <code className={classNames.code}>x = 42.</code>
         <p className={classNames.p}>
-          This always declares a global variable. It generates a strict JavaScript warning.
-          You shouldn't use this variant.
+          {
+          `This always declares a global variable. It generates a strict JavaScript warning.
+          You shouldn't use this variant.`
+          }
         </p>
         <p className={classNames.p}>With the keyword let. For example,</p>
         <code className={classNames.code}>let y = 13.</code>
