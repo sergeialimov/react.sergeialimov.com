246e2781f53dc07c8b9b0fd20652b9066a34f673
Sergei Alimov <8912652@gmail.com>
2020-07-14 15:07:31 +0300

    remove unused exports

--- src/scenes/Projects/components/index.js
+++ src/scenes/Projects/components/index.js
@@ -1,5 +1,1 @@
-// export { default as ProductLanding } from './Product-landing';
-// export { default as SurveyForm } from './SurveyForm';
-// export { default as TechDoc } from './TechDoc';
-// export { default as TributePage } from './TributePage';
 export { default } from './Project';
